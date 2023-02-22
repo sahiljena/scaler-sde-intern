e70c4a155fdb7802bcf76142f342ca4893e69a72
sahiljena <sahiljena@protonmail.com>
2023-02-22 23:07:44 +0530

    added comments to backend

--- backend/routes/interview.js
+++ backend/routes/interview.js
@@ -6,6 +6,7 @@ const nodemailer = require("nodemailer");
 const { rawListeners } = require("../models/Participant");
 
 const smtpTransport = nodemailer.createTransport({
+  // setup gmailer service
   service: "Gmail",
   auth: {
     user: "find.roomy.otp@gmail.com",
@@ -18,10 +19,12 @@ const smtpTransport = nodemailer.createTransport({
 
 const sendMassMail = async (subject, context, participants) => {
   const sendMailsTo = await Participant.find({
+    // find all participants details to send mail to
     _id: { $in: participants },
   });
   console.log(sendMailsTo);
   for (var i = 0; i < sendMailsTo.length; i++) {
+    // tarverse through and send mail one by one
     const mailOptions = {
       from: "find.roomy.otp@gmail.com",
       to: sendMailsTo[i].email,
@@ -29,6 +32,7 @@ const sendMassMail = async (subject, context, participants) => {
       text: context,
     };
     smtpTransport.sendMail(mailOptions, function (error, response) {
+      // mailing function
       if (error) {
         console.log(error);
       } else {
@@ -39,12 +43,13 @@ const sendMassMail = async (subject, context, participants) => {
 };
 
 function validateTime(start, end) {
+  // date time validation
   const startDate = new Date(start);
   const endDate = new Date(end);
   let todayDateTime = new Date();
   if (
-    startDate.getTime() > endDate.getTime() ||
-    startDate.getTime() < todayDateTime.getTime()
+    startDate.getTime() > endDate.getTime() || // to check if start time is greater than the end time
+    startDate.getTime() < todayDateTime.getTime() // start time should not be in the past
   ) {
     return false;
   }
@@ -54,8 +59,11 @@ function validateTime(start, end) {
 router.post("/new", async (req, res) => {
   // basic checks
   if (validateTime(req.body.startTime, req.body.endTime)) {
+    // validate date time
     if (req.body.participants.length >= 2) {
+      // check no. of particpants
       const intersetingInterviews = await Interview.find(
+        // find  overlapping interviews
         {
           $or: [
             { startTime: { $gte: req.body.startTime, $lt: req.body.endTime } },
@@ -64,10 +72,11 @@ router.post("/new", async (req, res) => {
         },
         async (err, interviews) => {
           if (err) {
-            res.status(400).json({ success: false, message: err });
+            res.status(400).json({ success: false, message: err }); // if checking fails
           } else {
-            let unavalibleParticipants = [];
+            let unavalibleParticipants = []; // to find overlapping inetrviews
             for (var i = 0; i < interviews.length; i++) {
+              // check if participants are the same or not
               for (var j = 0; j < req.body.participants.length; j++) {
                 if (
                   interviews[i].participants.includes(req.body.participants[j])
@@ -77,11 +86,12 @@ router.post("/new", async (req, res) => {
                     startTime: interviews[i].startTime,
                     endTime: interviews[i].endTime,
                   };
-                  unavalibleParticipants.push(tp);
+                  unavalibleParticipants.push(tp); // if same push into an array with their overlaping date time
                 }
               }
             }
             if (unavalibleParticipants.length == 0) {
+              // if no overlaping interviews then create new interview
               const interview = new Interview({
                 title: req.body.title,
                 startTime: req.body.startTime,
@@ -95,6 +105,7 @@ router.post("/new", async (req, res) => {
                   .save()
                   .then(() => {
                     sendMassMail(
+                      // send mass mail to all participants
                       "Interview Scheduled",
                       "An Interview has been Scheduled for you from " +
                         req.body.startTime +
@@ -103,28 +114,32 @@ router.post("/new", async (req, res) => {
                       req.body.participants
                     );
                     res.status(201).json({
+                      // return status to success
                       success: true,
                       message: "CREATED",
                     });
                   })
                   .catch((error) => {
-                    res.status(400).json({ message: error });
+                    res.status(400).json({ success: false, message: error }); // return for unsuccess full
                   });
               } catch (error) {
                 res
                   .status(400)
-                  .json({ success: false, message: "ERR_CREATING_INTERVIEW" });
+                  .json({ success: false, message: "ERR_CREATING_INTERVIEW" }); // return for unsuccess full
               }
             } else {
+              // if overlaping interviews then find particpant details
               // console.log(unavalibleParticipants);
               const unAvlPartArray = [];
               for (var i = 0; i < unavalibleParticipants.length; i++) {
                 unAvlPartArray.push(unavalibleParticipants[i].uid);
               }
+              // get thier details from mongoid
               const unAvlParticipantsDetails = await Participant.find({
                 _id: { $in: unAvlPartArray },
               });
               // console.log(unAvlParticipantsDetails);
+              // update the un-avl participants return structure with details like email,name
               for (var i = 0; i < unAvlParticipantsDetails.length; i++) {
                 for (var j = 0; j < unavalibleParticipants.length; j++) {
                   if (
@@ -140,6 +155,7 @@ router.post("/new", async (req, res) => {
               }
               console.log(unavalibleParticipants);
               res.status(400).json({
+                // return the error and the un-avl participants
                 success: false,
                 message: "CLASH",
                 unAvalibleParticipants: unavalibleParticipants,
@@ -154,12 +170,14 @@ router.post("/new", async (req, res) => {
         });
     } else {
       res.status(400).json({
+        // return if participants less than 2
         success: false,
         message: "LESS_PARTICIPANTS",
       });
     }
   } else {
     res.status(400).json({
+      // return if date validation fails
       success: false,
       message: "DATE_ERROR",
     });
@@ -168,30 +186,32 @@ router.post("/new", async (req, res) => {
 
 router.get("/all", async (req, res) => {
   try {
-    const allInterviews = await Interview.find();
+    const allInterviews = await Interview.find(); // find all interviews
     for (var i = 0; i < allInterviews.length; i++) {
+      // get the participant details to add into return structure
       let ids = allInterviews[i].participants;
       const participants = await Participant.find({ _id: { $in: ids } });
       allInterviews[i].participants = participants;
     }
     //console.log(allParticpants);
-    res.json({ success: true, interviews: allInterviews });
+    res.json({ success: true, interviews: allInterviews }); // return with all details
   } catch (error) {
     console.log(error);
-    res.status(500).json({ success: false, message: error.message });
+    res.status(500).json({ success: false, message: error.message }); // return if error
   }
 });
 
 router.delete("/delete/:id", async (req, res) => {
   console.log(req.params.id);
   const deletedInterveiw = await Interview.findOneAndDelete(
+    // find the document(interveiw) to delete
     { _id: req.params.id },
     function (err) {
       if (err) {
         console.log("Error");
-        res.status(400).json({ success: false, message: "NOT_DELETED" });
+        res.status(400).json({ success: false, message: "NOT_DELETED" }); // return if deleted
       } else {
-        res.status(200).json({ success: true, message: "DELETED" });
+        res.status(200).json({ success: true, message: "DELETED" }); // return if not deleted
       }
     }
   )
@@ -205,8 +225,11 @@ router.put("/update/:id", async (req, res) => {
   const iid = req.params.id;
 
   if (validateTime(req.body.startTime, req.body.endTime)) {
+    // validate date and time
     if (req.body.participants.length >= 2) {
+      // check for number of participants
       const intersetingInterviews = await Interview.find(
+        // find the all overlaping interviews
         {
           $or: [
             { startTime: { $gte: req.body.startTime, $lt: req.body.endTime } },
@@ -215,19 +238,21 @@ router.put("/update/:id", async (req, res) => {
         },
         async (err, interviews) => {
           if (err) {
-            res.status(400).json({ message: err });
+            res.status(400).json({ message: err }); // return on finding error while finding overlapping interveiws
           } else {
-            console.log(interviews);
+            //console.log(interviews);
             if (
-              (interviews.length === 1 && interviews[0]._id == iid) ||
-              interviews.length === 0
+              (interviews.length === 1 && interviews[0]._id == iid) || // check if it is the same interview(one we are trying to update)
+              interviews.length === 0 // check if there are no overlapping interviews
             ) {
               const interview = Interview.findOneAndUpdate(
+                // find by _id and update
                 { _id: iid },
                 req.body,
                 function (err, doc) {
                   if (err) return res.send(500, { error: err });
                   sendMassMail(
+                    // updation mass mail to all the candidates
                     "Interview Schedule Update",
                     "An Interview Schedule has been updated for you from " +
                       req.body.startTime +
@@ -241,7 +266,9 @@ router.put("/update/:id", async (req, res) => {
                 }
               );
             } else {
+              // if there are overlapping interviews
               let unavalibleParticipants = [];
+              // finding if the participants are the un-avl
               for (var i = 0; i < interviews.length; i++) {
                 for (var j = 0; j < req.body.participants.length; j++) {
                   if (
@@ -259,14 +286,18 @@ router.put("/update/:id", async (req, res) => {
                   }
                 }
               }
+              // un-avl participants mongoid in an array
               const unAvlPartArray = [];
               for (var i = 0; i < unavalibleParticipants.length; i++) {
                 unAvlPartArray.push(unavalibleParticipants[i].uid);
               }
+              // un-avl participants details
               const unAvlParticipantsDetails = await Participant.find({
                 _id: { $in: unAvlPartArray },
               });
+
               // console.log(unAvlParticipantsDetails);
+              // updating and populating structure with details
               for (var i = 0; i < unAvlParticipantsDetails.length; i++) {
                 for (var j = 0; j < unavalibleParticipants.length; j++) {
                   if (
@@ -281,13 +312,16 @@ router.put("/update/:id", async (req, res) => {
                 }
               }
 
+              // if there are no un-avl cparticipants then update
               if (unavalibleParticipants.length === 0) {
                 const interview = Interview.findOneAndUpdate(
+                  // find by mongoID and update
                   { _id: iid },
                   req.body,
                   function (err, doc) {
                     if (err) return res.send(500, { error: err });
                     sendMassMail(
+                      // mass mail all
                       "Interview Schedule Update",
                       "An Interview Schedule has been updated for you from " +
                         req.body.startTime +
@@ -295,13 +329,13 @@ router.put("/update/:id", async (req, res) => {
                         req.body.endTime,
                       req.body.participants
                     );
-                    return res
+                    return res // return if updated
                       .status(202)
                       .json({ success: true, message: "UPDATED" });
                   }
                 );
               } else {
-                console.log(unavalibleParticipants);
+                console.log(unavalibleParticipants); // return if candidates un-avl
                 res.status(400).json({
                   success: false,
                   message: "CLASH",
@@ -318,12 +352,14 @@ router.put("/update/:id", async (req, res) => {
         });
     } else {
       res.status(400).json({
+        // return if participants less than 2
         success: false,
         message: "LESS_PARTICIPANTS",
       });
     }
   } else {
     res.status(400).json({
+      // return if date time validation fails
       success: false,
       message: "DATE_ERROR",
     });
--- backend/routes/participants.js
+++ backend/routes/participants.js
@@ -4,12 +4,12 @@ const Participant = require("../models/Participant");
 
 router.get("/all", async (req, res) => {
   try {
-    const allParticpants = await Participant.find();
+    const allParticpants = await Participant.find(); // find all participants
     //console.log(allParticpants);
-    res.json(allParticpants);
+    res.json(allParticpants); // return found particpants
   } catch (error) {
     console.log(error);
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ message: error.message }); // return on error
   }
 });
 
